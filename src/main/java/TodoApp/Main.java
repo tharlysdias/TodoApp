690a3149e327dbec19dd0900513fb726abb2fe4a
Tharlys Dias <tharlys.souza@outlook.com>
2021-02-12 22:01:41 -0300

    Iniciando projeto TodoApp com Java

---
+++ src/main/java/TodoApp/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TodoApp;
+
+/**
+ *
+ * @author tharlys
+ * Gerenciador de dependencias
+ * Criar as classes do modelo:
+ * 1 - Tarefa; 2 - Projeto; (Extras: 3 - Tags; 4 - User)
+ * Classes de modelo, quando se constroi uma aplicação em java, segue-se um
+ * padrão de projeto (padronização na construção)
+ * Padrão MVC = Model View Controler: Tem-se classes que representam os modelos
+ * (models), são que representam as entidades que temos dentro do sistema
+ * View: Interface Grafica (visualização), são as telas (classes de interface)
+ * Controler: Classes que controlam a aplicação, tanto a interação do banco com
+ * a tela, como também faz controles na aplicação (diversos)
+ * Existem outros padrões de projeto que podem ser aplicados
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
